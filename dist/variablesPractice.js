fd65ba2d56ee2407e78c6b84ed0f22a61a8b5565
Administrator <Rahman2Sazal@github.com>
2026-08-13 00:18:30 +0200

    done for the day

--- dist/variablesPractice.js
+++ dist/variablesPractice.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Trying out variables in TypeScript!");
 let age = 30;
 let name = "Alice";
-let course = "TypeScript Basics";
+let course = "TypeScript Basics"; // string
 let largeNumber = 123456_789012_34567890; // BigInt 
 console.log("Age:", age);
 console.log("Name:", name);
