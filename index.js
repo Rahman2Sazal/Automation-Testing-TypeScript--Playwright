73f2aa77410781e32381be838ccd75492af231b9
Administrator <Rahman2Sazal@github.com>
2026-08-12 22:16:17 +0200

    configuration going on

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 "use strict";
 console.log("Hello, TypeScript!");
+let age = 25;
+let name1 = "John Doe";
--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 console.log("Hello, TypeScript!");
+
+let age: number = 25;
+let name1: string = "John Doe";
