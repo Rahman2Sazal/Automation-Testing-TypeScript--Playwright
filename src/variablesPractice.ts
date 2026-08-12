46c2ddbdf35a91dcb3c17edcb450fb77a197e2bf
Administrator <Rahman2Sazal@github.com>
2026-08-13 00:07:14 +0200

    variables done

--- src/variablesPractice.ts
+++ src/variablesPractice.ts
@@ -2,7 +2,7 @@ console.log("Trying out variables in TypeScript!");
 
 let age = 30;
 let name = "Alice";
-let course = "TypeScript Basics";
+let course = "TypeScript Basics";// string
 let largeNumber = 123456_789012_34567890; // BigInt 
 
 console.log("Age:", age);
