289f73dbf2eab1ac74d44fedbc01fe934b266484
Administrator <Rahman2Sazal@github.com>
2026-08-12 22:33:32 +0200

    setting TS

--- index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-console.log("Hello, TypeScript!");
-let age = 25;
-let name1 = "John Doe";
---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello, TypeScript!");
+let age = 25;
+let name1 = "John Doe";
+//# sourceMappingURL=index.js.map
